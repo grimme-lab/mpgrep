debaf3131f4568b7f999ccec6e70c7a5e5f70a95
Polt <seidler118@gmail.com>
2024-11-22 13:08:41 +0100

    script almost completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mpgrep
 Python script using the Materials Project REST API to compound and structure data from the Materials Project database, based on a given list of IDs. 
+
+# Usage
+`python mpgrep.py [ids file or single id]`
--- mpgrep/mpgrep.py
+++ mpgrep/mpgrep.py
@@ -1,30 +1,92 @@
+import argparse
+import configparser
+import os
+import sys
 from mp_api.client import MPRester
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.io.cif import CifWriter
-import argparse
 
 
 def parse():
+    def is_valid_string_or_file(arg):
+        if os.path.isfile(arg):
+            return arg
+        elif isinstance(arg, str) or arg == "":
+            return arg
+        else:
+            raise argparse.ArgumentTypeError(f"Invalid value: {arg}")
+
+
     parser = argparse.ArgumentParser(
-        description="Download CIF files from Materials Project using the API."
+        description="Download CIF files from Materials Project using the new REST API."
     )
-    parser.add_argument("id", help="The Materials Project compound ID (e.g., 'mp-149')")
+    parser.add_argument("input", type=is_valid_string_or_file, help="The Materials Project compound ID (e.g., 'mp-149') or the path to a file containing a list of IDs.", nargs="?", default=None)
     parser.add_argument(
-        "-p", dest="primitive", action="store_true", help="Output primitive cell"
+        "-p", dest="primitive", action="store_true", help="Outputs the primitive cell."
     )
+    parser.add_argument("--key", type=str, help="Stores your API key.")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
+    # Parse cml args
     args = parse()
 
-    with MPRester("xHFydzd3lHUM0LSA2CEXd8OSvyooGgLI") as mpr:
-        structure = mpr.get_structure_by_material_id(args.id)
-        sga = SpacegroupAnalyzer(structure)
+    # Setup the configuration
+    config = configparser.ConfigParser()
 
-        if args.primitive:
-            sym_structure = sga.get_primitive_standard_structure()
-            sym_structure.to(filename=f"{args.id}.cif")
+    # Try to read the config file in the home dir
+    rcpath = os.path.expanduser("~/.mpgreprc")
+    if not os.path.isfile(rcpath):
+        if args.key:
+            config["API"] = {"key": args.key}
+
+            # If the file does not exist create a new one and exit
+            with open(rcpath, "w") as f:
+                config.write(f)
+
+            print("API key stored.")
         else:
-            sym_structure = sga.get_conventional_standard_structure()
-            CifWriter(struct=sym_structure, symprec=1e-5).write_file(f"{args.id}.cif")
+            print("Please configure your Materials Project API access.")
+            sys.exit(0)
+    else:
+        # Read the existing config file
+        config.read(rcpath)
+
+        # If value in config file is empty (like if the user didn't modify it) exit
+        if config["API"]["key"] == "":
+            print("Please configure your Materials Project API access.")
+            sys.exit(0)
+
+    if not args.input:
+        print("Please provide an input (either a Materials Project ID or a file containing ID(s).")
+        sys.exit(0)
+
+    # Connect to the MP API
+    with MPRester(config["API"]["key"]) as mpr:
+        # If a file was given as input try to read all the IDs from that file
+        if os.path.isfile(args.input):
+            with open(args.input, "r") as f:
+                lines = f.readlines()
+
+            ids = [line.strip() for line in lines]
+        else:
+            # Assume a single ID was given as input
+            ids = [args.input]
+
+        for id in ids:
+            print(f"Fetching {id} ...")
+
+            structure = mpr.get_structure_by_material_id(id)
+            if structure == []:
+                print(f"Could not find structure {id}.")
+                continue
+
+            sga = SpacegroupAnalyzer(structure)
+
+            if args.primitive:
+                sym_structure = sga.get_primitive_standard_structure()
+                sym_structure.to(filename=f"{id}.cif")
+            else:
+                sym_structure = sga.get_conventional_standard_structure()
+                CifWriter(struct=sym_structure, symprec=1e-5).write_file(f"{id}.cif")
